9db6cbb930b8be52e4735d0ac20ef23964c580f4
sebastianbocul <seba23495@gmail.com>
2022-12-19 21:57:39 +0100

    Created get block and gas requests

--- app/src/main/java/com/sebix/testinfuraapi/MainActivity.kt
+++ app/src/main/java/com/sebix/testinfuraapi/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.sebix.testinfuraapi
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -8,14 +10,17 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.sebix.testinfuraapi.ui.theme.TestInfuraAPITheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.http.HttpService
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Column {
-                        var text by rememberSaveable { mutableStateOf("Text") }
+                        var text by rememberSaveable { mutableStateOf("") }
                         Row(Modifier.fillMaxWidth()) {
                             TextField(
                                 value = text,
@@ -36,17 +41,25 @@ class MainActivity : ComponentActivity() {
                                     text = it
                                 },
                                 label = {
-                                    Text("Enter network endpoint (eg. https://mainnet.infura.io/v3/xxxxxxxxxx")
+                                    Text("Enter network endpoint eg. https://mainnet.infura.io/v3/xxxxxxxxxx")
                                 }
                             )
                         }
                         Button(
-                            onClick = { runRequest() },
+                            onClick = { runBlockRequest(this@MainActivity, text) },
                             modifier = Modifier
-                                .weight(1f)
-                                .fillMaxWidth()
+                                .width(100.dp)
+                                .height(100.dp)
                         ) {
-                            Text(text = "Kliknij mnie")
+                            Text(text = "Get block number")
+                        }
+                        Button(
+                            onClick = { runGasRequest(this@MainActivity, text) },
+                            modifier = Modifier
+                                .width(100.dp)
+                                .height(100.dp)
+                        ) {
+                            Text(text = "Get gas")
                         }
                     }
                 }
@@ -55,19 +68,49 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-fun runRequest(){
+val ioScope = CoroutineScope(Dispatchers.IO)
+val mainScope = CoroutineScope(Dispatchers.Main)
 
+fun runBlockRequest(context: Context, apiUrl: String) {
+    ioScope.launch {
+        try {
+            val web3j =
+                Web3j.build(HttpService(apiUrl))
+            val blockNumber = web3j.ethBlockNumber().sendAsync().get().blockNumber
+            mainScope.launch {
+                Toast.makeText(context, "Block number: $blockNumber", Toast.LENGTH_SHORT).show()
+            }
+        }catch (e:Exception){
+            e.printStackTrace()
+            mainScope.launch {
+                Toast.makeText(context, "An error has occurred.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun runGasRequest(context: Context, apiUrl: String) {
+    ioScope.launch {
+        try {
+            val web3j =
+                Web3j.build(HttpService(apiUrl))
+            val gas = web3j.ethGasPrice().sendAsync().get().gasPrice
+            val gasDouble:Double = gas.toDouble() / 1000000000.0
+            mainScope.launch {
+                Toast.makeText(context, "Gas: ${gasDouble}", Toast.LENGTH_SHORT).show()
+            }
+        }catch (e:Exception){
+            e.printStackTrace()
+            mainScope.launch {
+                Toast.makeText(context, "An error has occurred.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TestInfuraAPITheme {
-        Greeting("Android")
     }
 }

ccddb8bc69d795d8f57f170d48d1fa53849358db
sebastianbocul <seba23495@gmail.com>
2023-04-02 18:55:51 +0200

    Added new example test

--- app/src/test/java/com/sebix/testinfuraapi/ExampleUnitTest.kt
+++ app/src/test/java/com/sebix/testinfuraapi/ExampleUnitTest.kt
@@ -14,4 +14,8 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+    @Test
+    fun addition_isNotCorrect() {
+        assertNotEquals(4, 4 + 2)
+    }
 }
